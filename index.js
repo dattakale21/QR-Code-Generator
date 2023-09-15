195fd4a9d8b97ed5530bc181498bae6b1bd8291d
dattakale21 <dattakale2008@gmail.com>
2023-09-15 21:31:39 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-
-
 let qrtext = document.querySelector('#qrtext');
 let imgbox = document.querySelector('#imgbox');
 let qrlimg = document.querySelector('#qrlimg');
@@ -10,10 +8,14 @@ function generateQR() {
         qrlimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
         imgbox.classList.add("show-img");
     }
-    else{
+    
+    else {
         qrtext.classList.add("error");
         setTimeout(() => {
             qrtext.classList.remove("error");
         }, 1000);
     }
 }
+
+
+// Code By - DATTA KALE
