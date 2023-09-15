459db90e8bd2684094ec0bc1a7c45fdfa760d4f8
dattakale21 <dattakale2008@gmail.com>
2023-09-15 21:21:53 +0530

    initial changes

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+let qrtext = document.querySelector('#qrtext');
+let imgbox = document.querySelector('#imgbox');
+let qrlimg = document.querySelector('#qrlimg');
+
+function generateQR() {
+    if (qrtext.value.length > 0) {
+        qrlimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+        imgbox.classList.add("show-img");
+    }
+    else{
+        qrtext.classList.add("error");
+        setTimeout(() => {
+            qrtext.classList.remove("error");
+        }, 1000);
+    }
+}
